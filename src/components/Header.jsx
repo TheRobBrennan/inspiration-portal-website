158776af8302147812b0da1ba5a1f97356adb178
Rob Brennan <rob@therobbrennan.com>
2024-03-05 13:30:32 -0800

    Remove commented import of the Brainwave SVG

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import { disablePageScroll, enablePageScroll } from 'scroll-lock'
 
-// import { brainwave } from '../assets'
 import { navigation } from '../constants'
 import Button from './Button'
 import MenuSvg from '../assets/svg/MenuSvg'
