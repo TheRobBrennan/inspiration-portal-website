fcecdc238f086152f218f39ffeb59eaa61773639
Rob Brennan <rob@therobbrennan.com>
2024-03-10 23:39:20 -0700

    Add the Plantagenet Cherokee font

--- apps/demo-02/app/page.tsx
+++ apps/demo-02/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main>
-
+    <main className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-center text-gray-900 font-sans">
+        [DEMO] Inspiration Portal
+      </h1>
     </main>
   );
 }
